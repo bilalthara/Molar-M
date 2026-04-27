a4046d607367433ffeffb4be41af57a530334d28
Muhammad Bilal <mu.bilal.ashraf@gmail.com>
2026-04-27 23:44:49 +0500

    fix new

--- components/layout/scroll-to-top.tsx
+++ components/layout/scroll-to-top.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export function ScrollToTop() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "auto" });
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
